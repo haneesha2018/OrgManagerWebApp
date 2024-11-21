2e8be62d82d2a6c4624b7dc59d0b6e5fd534c640
Haneesha Dushara <haneesha207@gmail.com>
2024-11-20 20:36:39 -0500

    Initialize Repo and First Commit

--- OrgManagerWebApp/Window1.Designer.cs
+++ OrgManagerWebApp/Window1.Designer.cs
@@ -29,12 +29,34 @@ namespace OrgManagerWebApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new Wisej.Web.Button();
+            this.progressBar1 = new Wisej.Web.ProgressBar();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
             // 
+            this.button1.Location = new System.Drawing.Point(234, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(234, 213);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 24);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Text = "progressBar1";
+            // 
+            // Window1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(613, 432);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
             this.Name = "Window1";
             this.Text = "Window1";
             this.ResumeLayout(false);
@@ -42,6 +64,9 @@ namespace OrgManagerWebApp
         }
 
         #endregion
+
+        private Wisej.Web.Button button1;
+        private Wisej.Web.ProgressBar progressBar1;
     }
 }
 
--- OrgManagerWebApp/Window1.cs
+++ OrgManagerWebApp/Window1.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Threading;
 using Wisej.Web;
 
 namespace OrgManagerWebApp
@@ -9,6 +10,34 @@ namespace OrgManagerWebApp
         public Window1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.StartTask(() =>
+            {
+                while (this.progressBar1.Value < 100)
+                {
+                    this.progressBar1.Value += 10;
+
+                    Thread.Sleep(1000);
+
+                    Application.Update(this);
+                }
+            }
+
+                );
+
+        }
+
+        private void Window1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
